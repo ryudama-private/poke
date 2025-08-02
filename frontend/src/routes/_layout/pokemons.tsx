9c02fc9191ec1c68bf4bc7c001dc773946417d27
ryudama-private <my_com_address@yahoo.co.jp>
2025-08-02 11:50:19 +0900

    テーブルに変換

--- frontend/src/routes/_layout/pokemons.tsx
+++ frontend/src/routes/_layout/pokemons.tsx
@@ -1,8 +1,6 @@
-import { Container, Heading } from "@chakra-ui/react";
-import { List, ListItem } from "@chakra-ui/layout";
+import { Container, Heading, Flex, Table, Badge } from "@chakra-ui/react";
 import { createFileRoute } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
-
 // @ts-ignore
 export const Route = createFileRoute("/_layout/pokemons")({
   component: Pokemons,
@@ -13,6 +11,45 @@ interface Pokemon {
   name: string;
   type1: string;
   type2?: string;
+}
+
+function PokemonsTable({ pokemons }: { pokemons: Pokemon[] }) {
+  return (
+    <Table.Root size={{ base: "sm", md: "md" }}>
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeader w="sm">画像</Table.ColumnHeader>
+          <Table.ColumnHeader w="sm">名前</Table.ColumnHeader>
+          <Table.ColumnHeader w="sm">タイプ1</Table.ColumnHeader>
+          <Table.ColumnHeader w="sm">タイプ2</Table.ColumnHeader>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {pokemons.map((pokemon) => (
+          <Table.Row key={pokemon.id}>
+            <Table.Cell>
+              <img
+                src={`/data/pokemon_images/${encodeURIComponent(pokemon.name)}.png`}
+                alt={pokemon.name}
+                style={{ width: "48px", height: "48px", objectFit: "contain" }}
+              />
+            </Table.Cell>
+            <Table.Cell>{pokemon.name}</Table.Cell>
+            <Table.Cell>
+              <Badge colorScheme="teal">{pokemon.type1}</Badge>
+            </Table.Cell>
+            <Table.Cell>
+              {pokemon.type2 ? (
+                <Badge colorScheme="purple">{pokemon.type2}</Badge>
+              ) : (
+                "-"
+              )}
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  );
 }
 
 function Pokemons() {
@@ -31,14 +68,7 @@ function Pokemons() {
       <Heading size="lg" pt={12}>
         Pokemons List
       </Heading>
-      <List spacing={4} pt={4}>
-        {pokemons.map((pokemon) => (
-          <ListItem key={pokemon.id}>
-            {pokemon.name} - {pokemon.type1}
-            {pokemon.type2 ? ` / ${pokemon.type2}` : ""}
-          </ListItem>
-        ))}
-      </List>
+      <PokemonsTable pokemons={pokemons} />
     </Container>
   );
 }
