5e58c1de6cba492d1ad2a8464627a6a41968268d
ryudama-private <my_com_address@yahoo.co.jp>
2025-08-10 10:53:08 +0900

    バージョン統一

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,9 @@ if not DATABASE_URL:
 
 # データベースエンジンを作成します。
 # `echo=True`は、開発中に実行されるSQLをコンソールに出力してデバッグに役立ちます。
-engine = create_engine(DATABASE_URL, echo=True)
+# engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
+
 
 # -----------------
 # データベースセッションの依存性注入のための関数
