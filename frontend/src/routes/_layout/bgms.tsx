c545eea68ed0dc64bb7b7b133d564924d7b570af
ryudama-private <my_com_address@yahoo.co.jp>
2025-08-11 10:23:04 +0900

    BGMurl更新

--- frontend/src/routes/_layout/bgms.tsx
+++ frontend/src/routes/_layout/bgms.tsx
@@ -23,7 +23,7 @@ function Bgms() {
   const [isMuted, setIsMuted] = useState(false);
 
   useEffect(() => {
-    fetch("http://localhost:8000/api/v1/bgms")
+    fetch("https://pokedex-back.azurewebsites.net/api/v1/bgms")
       .then((res) => res.json())
       .then((data) => {
         setBgms(data);
