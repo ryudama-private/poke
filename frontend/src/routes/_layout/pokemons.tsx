137b0e01280188ccdd681e433faa37e29c720651
ryudama-private <my_com_address@yahoo.co.jp>
2025-08-11 11:00:12 +0900

    pokemonsurl更新

--- frontend/src/routes/_layout/pokemons.tsx
+++ frontend/src/routes/_layout/pokemons.tsx
@@ -56,7 +56,7 @@ function Pokemons() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/api/v1/pokemons")
+    fetch("https://pokedex-back.azurewebsites.net/api/v1/pokemons")
       .then((res) => res.json())
       .then((data) => {
         setPokemons(data);
