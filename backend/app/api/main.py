c06c53d98c98fdd8efb0e5db956c506b95716705
ryudama-private <my_com_address@yahoo.co.jp>
2025-08-17 23:22:06 +0900

    chatコメントアウトでテスト

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, private, users, utils, bgm, pokemon, chat
+from app.api.routes import items, login, private, users, utils, bgm, pokemon
+# from app.api.routes import chat
+
 from app.core.config import settings
 
 api_router = APIRouter()
@@ -10,7 +12,7 @@ api_router.include_router(utils.router)
 api_router.include_router(items.router)
 api_router.include_router(bgm.router, prefix="/bgms", tags=["bgms"])
 api_router.include_router(pokemon.router, prefix="/pokemons", tags=["pokemons"])
-api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
+# api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
 
 
 
